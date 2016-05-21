6b3653d378553106828f3e28c5a776e20505b3a3
Stephen Ierodiaconou <stevegeek@gmail.com>
2016-05-21 11:40:59 +0100

    Remove unused method

--- src/XeroInvoiceService.js
+++ src/XeroInvoiceService.js
@@ -24,21 +24,16 @@ class XeroInvoiceService {
   }
 
   items() {
-    console.log('Fetching invoices from Xero...');
+    console.info('Fetching invoices from Xero...');
     return this._client.callAsync('GET', '/Invoices', null).then((response) => {
       const invoices = response.Response.Invoices;
       // Normalise output from xero, always have an array in .Invoice
       if (invoices && invoices.Invoice && typeof invoices.Invoice.length == 'undefined') {
         invoices.Invoice = [invoices.Invoice];
       }
-      console.log(`Fetched ${(invoices && invoices.Invoice.length) || 0} invoices from Xero`);
+      console.info(`Fetched ${(invoices && invoices.Invoice.length) || 0} invoices from Xero`);
       return invoices && invoices.Invoice;
     });
-  }
-
-  item(id) {
-    console.log(`GET INVOICE ${id}`); return Promise.resolve();
-    //return this._client.callAsync('GET', `/Invoices/${id}`, null);
   }
 
   // 'private' methods
